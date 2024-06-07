25a20ad9b696e560c553bd10a9ed0faf380b7ca1
pandeysh-25 <pandeysh25@gmail.com>
2024-06-07 14:44:58 +0530

    Init commit

---
+++ maven-arx/src/main/java/ARXTesting.java
@@ -0,0 +1,122 @@
+import org.deidentifier.arx.*;
+import org.deidentifier.arx.aggregates.HierarchyBuilder;
+import org.deidentifier.arx.aggregates.HierarchyBuilderIntervalBased;
+import org.deidentifier.arx.aggregates.StatisticsFrequencyDistribution;
+import org.deidentifier.arx.criteria.KAnonymity;
+
+import java.util.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class ARXTesting {
+    public static void main(String[] args) throws IOException {
+
+        Data dataset = Data.create("/Users/shashankpandey/IdeaProjects/arx-attempt/maven-arx/src/main/java/categoricalDatasetProcessed_fin_TEL_bounds_corrected_1_22.csv", Charset.defaultCharset(), ',');
+
+
+        HierarchyBuilderIntervalBased<Double> builder_long = HierarchyBuilderIntervalBased.create(DataType.DECIMAL);
+
+        for (double i = 77.0; i<82.0; i+=0.5){
+            builder_long.addInterval(i,i+0.5);
+        }
+
+        builder_long.getLevel(0).addGroup(1);
+        builder_long.getLevel(1).addGroup(2);
+        builder_long.getLevel(2).addGroup(2);
+        builder_long.getLevel(3).addGroup(2);
+
+        HierarchyBuilderIntervalBased<Double> builder_lat = HierarchyBuilderIntervalBased.create(DataType.DECIMAL);
+
+        for (double i = 15.0; i<20.0; i+=0.5){
+            builder_lat.addInterval(i,i+0.5);
+        }
+
+        builder_lat.getLevel(0).addGroup(1);
+        builder_lat.getLevel(1).addGroup(2);
+        builder_lat.getLevel(2).addGroup(2);
+        builder_lat.getLevel(3).addGroup(2);
+
+        String[] identifying = new String[]{"Record","FarmerName","LandArea","KhasraNo./DagNo.","SurveyNo"};
+
+        for (int i = 0; i<identifying.length; i++){
+            dataset.getDefinition().setAttributeType(identifying[i],AttributeType.IDENTIFYING_ATTRIBUTE);
+        }
+
+        String[] insensitive = new String[]{"SampleNo.","village","pH","EC","OC","N","P","K","S","Zn","Fe","Cu","Mn","B","state","district","subdistrict","cycle"};
+
+        for (int i = 0; i<insensitive.length; i++){
+            dataset.getDefinition().setAttributeType(insensitive[i],AttributeType.INSENSITIVE_ATTRIBUTE);
+        }
+
+        dataset.getDefinition().setAttributeType("Longitude", builder_long);
+        dataset.getDefinition().setAttributeType("Latitude", builder_lat);
+
+        ARXConfiguration config = ARXConfiguration.create();
+        config.addPrivacyModel(new KAnonymity(5));
+        config.setSuppressionLimit(0.00d);
+
+        ARXSolverConfiguration.create();
+
+        ARXAnonymizer anonymizer = new ARXAnonymizer();
+
+        ARXResult result = anonymizer.anonymize(dataset, config);
+
+        DataHandle handle = result.getOutput();
+        DataHandle view = handle.getView();
+
+        System.out.println(view);
+
+        StatisticsFrequencyDistribution districtRes = view.getStatistics().getFrequencyDistribution(22);
+
+        String[] sortedVals  = districtRes.values;
+        double[] sortedCounts = districtRes.frequency;
+
+        List<String> valList = new ArrayList<>();
+        List<Double> countList = new ArrayList<>();
+
+        List<Pair<String, Double>> pairList = new ArrayList<>();
+        //List<OutputVals> dist_freq = new List<OutputVals>();
+
+
+        //Map<String, Double> dist_freq= new HashMap<String, Double>();
+        for(int i = 0; i<sortedVals.length; i++){
+           // dist_freq.put(sortedVals[i],sortedCounts[i]);
+            valList.add(sortedVals[i]);
+            countList.add(sortedCounts[i]);
+            pairList.add(new Pair<>(sortedVals[i],sortedCounts[i]));
+
+        }
+        Collections.sort(pairList, Comparator.comparing(Pair::getValue));
+
+        List<String> sortedValues = new ArrayList<>();
+        List<Double> sortedCountss = new ArrayList<>();
+
+        for (Pair<String, Double> pair : pairList) {
+            sortedValues.add(pair.getKey());
+            sortedCountss.add(pair.getValue()*districtRes.count);
+
+            System.out.println(pair.getKey());
+            System.out.println(pair.getValue()*districtRes.count);
+        }
+
+    }
+
+    static class Pair<K, V> {
+        private final K key;
+        private final V value;
+
+        Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+}
